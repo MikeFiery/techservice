208daac288d06526a24989c5ea58dc7cf9edeabb
Mike Fiery <miguelangeloneves96@gmail.com>
2026-07-29 18:38:45 +0100

    inserir cliente

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-from src.repositories.cliente_repository import listar
+from src.models.cliente import Cliente
+from src.repositories.cliente_repository import inserir, listar
 
 
 def main():
     print("=== TechService - Sistema de Gestão de Assistência Técnica ===")
 
+    cliente = Cliente(
+        nome="Cliente Teste",
+        email="teste@email.pt",
+        telefone="910000000"
+    )
+
+    cliente = inserir(cliente)
+    print(f"Cliente gravado na base de dados. ID: {cliente.id_cliente}")
+
     print("\nClientes ativos:")
-
-    clientes = listar()
-
-    for item in clientes:
+    for item in listar():
         print(
             item["id_cliente"],
             item["nome"],
