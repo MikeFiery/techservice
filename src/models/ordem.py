f6152ae110cd45c222c7adb34197df89c05feac6
Mike Fiery <miguelangeloneves96@gmail.com>
2026-07-31 15:56:06 +0100

    final class ordem

--- src/models/ordem.py
+++ src/models/ordem.py
@@ -1,14 +1,16 @@
 class Ordem:
     def __init__(self, id_ordem=None, id_equipamento=None,
-    diagnostico="", status="Em andamento", data_abertura=None, data_fechamento=None,
-    defeito="", servico="", valor=0.0):
+    diagnostico="", status="Em andamento", solucao="", data_abertura=None, prioridade="",
+    valor_servico=0.0, valor_pecas=0.0, valor_total=0.0, desconto=0.0,observacoes=""):
         self.id_ordem = id_ordem
         self.id_equipamento = id_equipamento
         self.diagnostico = diagnostico
         self.status = status
+        self.solucao = solucao
         self.data_abertura = data_abertura
-        self.data_fechamento = data_fechamento
-        self.defeito = defeito
-        self.servico = servico
-        self.valor = valor
-
+        self.prioridade = prioridade
+        self.valor_servico = valor_servico
+        self.valor_pecas = valor_pecas
+        self.valor_total = valor_total
+        self.desconto = desconto
+        self.observacoes = observacoes
